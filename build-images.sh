ee5391ba66494853c8083890e18fa3262a94eb85
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-07-10 11:05:48 -0400

    Release ghr v0.12.2.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 0.12/Dockerfile -t cibuilds/github:latest -t cibuilds/github:0.12.1  -t cibuilds/github:0.12 .
+docker build --file 0.12/Dockerfile -t cibuilds/github:latest -t cibuilds/github:0.12.2  -t cibuilds/github:0.12 .
