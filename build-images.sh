2190ecaddda128be0b1556e07b1fcf2496582590
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-09-17 16:17:50 -0400

    Add ghr v0.13.0.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 0.12/Dockerfile -t cibuilds/github:latest -t cibuilds/github:0.12.2  -t cibuilds/github:0.12 .
+docker build --file 0.13/Dockerfile -t cibuilds/github:latest -t cibuilds/github:0.13.0  -t cibuilds/github:0.13 .
